7e594e10971bc6e8955017f0e70b6f0721822539
Timothy Day <the.tday@gmail.com>
2014-09-14 12:44:26 -0400

    Baby state text fades after a few seconds.

--- Assets/Scripts/Debug/babyStateDisplay.cs
+++ Assets/Scripts/Debug/babyStateDisplay.cs
@@ -2,16 +2,29 @@
 using System.Collections;
 
 public class babyStateDisplay : MonoBehaviour {
-	
 	public RespiratoryCase respiratoryCase;
 	int state;
+	public dfLabel lbl;
+
+	void Start() {
+		StartCoroutine(Fade ());
+	}
+
+	IEnumerator Fade() {
+		float delay = 4f;
+		yield return new WaitForSeconds(delay);
+		float t = 0;
+		while(t < 2f) {
+			t += Time.deltaTime;
+			lbl.Opacity = 1 - t / 2f;
+			yield return null;
+		}
+	}
 	
 	// diaplay the state of baby with gui lable
-	void Update(){
-		state = CaseInitializer.Instance.ActiveCase.currentState;
-		
-		switch(state)
-		{	
+	void Update() {
+		state = CaseInitializer.Instance.ActiveCase.currentState;	
+		switch(state) {	
 		case 0:
 			GetComponent<dfLabel>().Text = "This baby isn't looking so good...";
 			break;
