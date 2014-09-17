ae7e8d0a8f41f9e69ccc6df25c0f9eab13e457b3
Timothy Day <the.tday@gmail.com>
2014-09-17 10:49:34 -0400

    Face exam.

---
+++ Assets/Scripts/UI/FaceExam.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceExam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void On() {
+		Camera.main.animation ["faceExam"].speed = 1f;
+		Camera.main.animation["faceExam"].time = 0;
+		Camera.main.animation.Play("faceExam");
+	}
+
+	public void Off() {
+		
+		Camera.main.animation ["faceExam"].speed = -1f;
+		Camera.main.animation["faceExam"].time = Camera.main.animation["faceExam"].length;
+		Camera.main.animation.Play("faceExam");
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
