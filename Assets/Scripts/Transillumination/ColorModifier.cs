465133945de9f9646a8a04b62270bd47b61419d5
CoryBerg <cory3bergquist@gmail.com>
2014-10-29 11:49:34 -0400

    Fixed transillum in Cardiac case

---
+++ Assets/Scripts/Transillumination/ColorModifier.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorModifier : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (CaseHandler.Instance.currentCase == NeonatalCase.Cardiac) {
+			this.light.color = Color.white;
+			this.light.intensity = 8.0f;
+		}
+	}
+
+}
