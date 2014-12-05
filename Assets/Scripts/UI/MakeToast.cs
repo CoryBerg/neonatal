23eba41437478a12eae7c80b9f825fd93e959b2f
Timothy Day <the.tday@gmail.com>
2014-12-05 12:23:58 -0500

    Toasts for administering the fluids.

---
+++ Assets/Scripts/UI/MakeToast.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeToast : MonoBehaviour {
+	public string toast;
+	
+	public void OnClick(dfControl control, dfMouseEventArgs mouseEvent) {
+		print ("Toasting");
+		TestHandler.Instance.Toast(toast);
+	}
+}
